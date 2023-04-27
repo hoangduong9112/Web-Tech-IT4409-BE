bbadb32949ae005729290d8fdfdaad6fedfc155c
Nguyen Nam Duong <DuongNNb@bkav.com>
2023-04-27 17:29:19 +0700

    .env config

--- config/Db.js
+++ config/Db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
-const dbConfig = require("./DbConfig.js");
+require('dotenv').config();
 
 var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  port: dbConfig.PORT,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = connection;
